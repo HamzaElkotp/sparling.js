440cf262d60dd11e0e60c00c27af9a80caecf6a1
Hamza Elkotb <61008779+HamzaElkotp@users.noreply.github.com>
2022-09-21 21:55:49 +0300

    make the pages in spa.json file order unnecessary
    by making loop and checking if "SPAname" in page object "in spa.json" is same to html element "SPAname" meta tag

--- main.js
+++ main.js
@@ -7,15 +7,26 @@ function jsonRead(jsonpages, metatagArr){
     }).then((data)=>{
         function evenAdder(){
 
+            // to make No need to stick to static "page title" like: "SPAtitle", "page url" like: "SPAurl" or "page name" like: "SPAname", in your json file, name them as you want but be carefull to their order
             let SPAname = metatagArr[0];
             let SPAurl = metatagArr[1];
             let SPAtitle = metatagArr[2];
 
             let elements = document.querySelectorAll('[SPAname]');
+
+            // Loop through "SPAname" meta tag elemnts to add to them 'click' event
             elements.forEach((ele, index)=>{
                 ele.addEventListener('click',()=>{
-                    document.title = data[index][SPAtitle];
-                    window.history.pushState("","",data[index][SPAurl]);
+
+                    // change title, url   regardless of pages or json file objects order
+                    data.forEach((item, dataI)=>{
+                        if(data[dataI][SPAname] == ele.getAttribute("SPAname")){
+                            document.title = data[dataI][SPAtitle];
+                            window.history.pushState("","",data[dataI][SPAurl]);
+                        }
+                    })
+
+                    
                 })
             })
         }
